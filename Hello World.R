8bc1b3d1ecaa466abf01b93c4de03e4e9002e9ab
Maxwell Wang <90732327+mwang840@users.noreply.github.com>
2022-03-11 15:08:13 -0500

    Add files via upload

---
+++ Hello World.R
@@ -0,0 +1,24 @@
+##library(tidyverse)
+##library(dslabs)
+
+##data(murders)
+
+##murders %>%
+## ggplot(aes(population, total, label = abb, color = region)) +
+## geom_label()
+
+
+library(dslabs)
+data(murders)
+
+
+data(heights)
+heights
+
+# installing and loading the dplyr package
+install.packages("dplyr")
+library(dplyr)
+x=5
+x^5
+z = x^0
+z
